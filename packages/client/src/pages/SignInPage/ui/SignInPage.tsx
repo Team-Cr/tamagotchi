ffde448e904e57e3f0d125adcb284f638205d3e1
Anna Shakhova <anuta199212@yandex.ru>
2022-11-28 02:09:27 +0700

    fixed grid

--- packages/client/src/pages/SignInPage/ui/SignInPage.tsx
+++ packages/client/src/pages/SignInPage/ui/SignInPage.tsx
@@ -5,22 +5,24 @@ import image from '@/shared/assets/images/gatito_paradax3.gif';
 const SignInPage = () => {
   return (
     <>
-      <div className={styles.title}>Welcome, catlover</div>
       <div className={styles.container}>
-        <div className={styles.column}>
+        <div className={styles.title}>Welcome, catlover</div>
+
+        <div className={styles.gatito}>
           <img src={image} alt='gatito' />
-          <div className={styles.signup}>
-            <button className={styles.button}>I want a new</button>
-          </div>
         </div>
-        <div className={styles.column}>
-          <div>
-            <Input name='login' type='text' value='' placeholder='Login' />
-            <Input name='password' type='password' value='' placeholder='Password' />
-          </div>
-          <div>
-            <button className={styles.button}>Visit my cat</button>
-          </div>
+
+        <div className={styles['new-gatito']}>
+          <button>I want a new</button>
+        </div>
+
+        <div className={styles.fields}>
+          <Input name='login' type='text' value='' placeholder='Login' />
+          <Input name='password' type='password' value='' placeholder='Password' />{' '}
+        </div>
+
+        <div className={styles.button}>
+          <button>Visit my cat</button>
         </div>
       </div>
     </>
