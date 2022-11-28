cb18c67249f6e37509d07e0f8d24ba9579271c93
Violet <eerfeer@yandex.ru>
2022-11-28 14:53:15 +0300

    Add passWithNoTests, update jest mappers

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -10,7 +10,8 @@ export default {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+    '^.+\\.(css|less|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
