2d670b548a158471bb961875e3b89de13a8d240b
Anna Shakhova <anuta199212@yandex.ru>
2022-11-28 21:06:57 +0700

    fixed

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,20 +1,8 @@
-import { useEffect } from 'react';
 import './styles/index.scss';
 
 import { SignInPage } from '@/pages/SignInPage/ui/SignInPage';
 
 export const App = () => {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-    };
-
-    fetchServerData();
-  }, []);
-
   return (
     <>
       <SignInPage />
