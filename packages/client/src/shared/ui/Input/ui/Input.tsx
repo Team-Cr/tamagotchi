23f1f4c25c74a3a2e876de728983effe45bc74a0
Anna <38813390+anuta199212@users.noreply.github.com>
2022-11-30 22:51:08 +0700

    Add login page layout (#5)
    * Add signin page layout
    
    * Update SignInPage.tsx
    
    * fixed styles after lint test
    
    * Update package.json
    
    * Fixed afer review
    
    * fixed after review
    
    * fixed lint
    
    * Update SignInPage.tsx

---
+++ packages/client/src/shared/ui/Input/ui/Input.tsx
@@ -0,0 +1,21 @@
+import { InputHTMLAttributes } from 'react';
+import styles from './Input.module.scss';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  type: 'text' | 'password';
+  value: string;
+  placeholder: string;
+  disabled?: boolean;
+}
+
+const Input = (props: InputProps) => {
+  return (
+    <>
+      <input className={styles.input} {...props} />
+    </>
+  );
+};
+
+export { Input };
