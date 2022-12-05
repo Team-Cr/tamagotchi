1a30ab80a8cab7f69e95ceaf55639192770075df
hosyaeuw <buyclock@mail.ru>
2022-12-05 22:57:49 +0300

    refactor: delete form button duplicate

--- packages/client/src/shared/ui/FormButton/ui/FormButton.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import styles from './styles.module.scss';
-
-type Props = Record<string, never>;
-
-export const FormButton: React.FC<React.PropsWithChildren<Props>> = ({ children }) => {
-  return <button className={styles.button}>{children}</button>;
-};
