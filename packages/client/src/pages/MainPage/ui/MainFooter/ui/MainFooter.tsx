10f0425eec954dc7c15eaf58c58fe0935c4239ec
Violet <eerfeer@yandex.ru>
2022-12-05 12:59:48 +0300

    Return icons

--- packages/client/src/pages/MainPage/ui/MainFooter/ui/MainFooter.tsx
+++ packages/client/src/pages/MainPage/ui/MainFooter/ui/MainFooter.tsx
@@ -1,12 +1,12 @@
-import LeaderboardSvg from '@/shared/assets/images/leaderboard.svg';
-import PreferencesSvg from '@/shared/assets/images/preferences.svg';
+import LeaderBoardIcon from '@/shared/assets/images/LeaderBoardIcon.png';
+import SettingsIcon from '@/shared/assets/images/SettingsIcon.png';
 import css from './MainFooter.module.scss';
 
 export const MainFooter = () => {
   return (
     <footer className={css.main__footer}>
-      <LeaderboardSvg />
-      <PreferencesSvg />
+      <img className={css.main__footer__image} src={LeaderBoardIcon} alt='LeaderBoardIcon' />
+      <img className={css.main__footer__image} src={SettingsIcon} alt='LeaderBoardIcon' />
     </footer>
   );
 };
