d323de9179ee98849dc1cdd1de3c36a02a339fd9
hosyaeuw <buyclock@mail.ru>
2022-12-06 20:54:51 +0300

    refactor: add early return in convertingDataToSend

--- packages/client/src/shared/lib/convertingDataToSend.ts
+++ packages/client/src/shared/lib/convertingDataToSend.ts
@@ -2,15 +2,12 @@ import { camelToSnakeCase } from './camelToSnakeCase';
 import { isObject } from './isObject';
 
 export const convertingDataToSend = (data: Record<string, unknown>) => {
-  if (isObject(data)) {
-    const typedData = data;
-    const result: Record<string, unknown> = {};
-    Object.keys(typedData).forEach((key) => {
-      result[camelToSnakeCase(key)] = typedData[key];
-    });
-
-    return result;
+  if (!isObject(data)) {
+    return data;
   }
 
-  return data;
+  return Object.keys(data).reduce((acc, key) => {
+    acc[camelToSnakeCase(key)] = data[key];
+    return acc
+  }, {} as Record<string, unknown>);
 };
