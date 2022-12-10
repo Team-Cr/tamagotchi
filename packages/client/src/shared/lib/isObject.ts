16789d002f1b276e19adef8d9af851b7e42b5c52
hosyaeuw <buyclock@mail.ru>
2022-12-10 21:51:30 +0300

    fix: delete not used isObject file

--- packages/client/src/shared/lib/isObject.ts
+++
@@ -1,8 +0,0 @@
-export const isObject = (value: unknown): boolean => {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    value.constructor === Object &&
-    Object.prototype.toString.call(value) === '[object Object]'
-  );
-};
