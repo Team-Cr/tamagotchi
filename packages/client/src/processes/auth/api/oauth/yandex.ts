ea8dc0d71c721ae8ea14160a0148455dad7f9d1e
Adel Zaripov <zaripovar1998@gmail.com>
2023-01-15 18:15:51 +0300

    add yandex oauth

--- packages/client/src/processes/auth/api/oauth/yandex.ts
+++ packages/client/src/processes/auth/api/oauth/yandex.ts
@@ -4,9 +4,10 @@ import { AxiosResponse } from '@/shared/lib/api/types/axios';
 import { redirect } from 'react-router-dom';
 
 const BASE_URL = '/oauth/yandex';
+const BASE_REDIRECT = `${__APP_URL__}/login`;
 
 const Routes = {
-  GET_SERVICE_ID: `${BASE_URL}/service_id`,
+  GET_SERVICE_ID: `${BASE_URL}/service-id`,
   AUTH: BASE_URL,
 }
 
@@ -15,13 +16,13 @@ type ServiceIdResponse = {
 }
 
 export const YandexOAuth = {
-  getServiceId: (redirect_uri: string = __APP_URL__): AxiosResponse<ServiceIdResponse> =>
+  getServiceId: (redirect_uri: string = BASE_REDIRECT): AxiosResponse<ServiceIdResponse> =>
     axiosInstance.get(Routes.GET_SERVICE_ID, {
       params: {
         redirect_uri,
       },
     }),
-  getOAuthLink: (client_id: string, redirect_uri: string =  __APP_URL__) : string => {
+  getOAuthLink: (client_id: string, redirect_uri: string = BASE_REDIRECT) : string => {
     const query = {
       response_type: 'code',
       client_id,
@@ -34,7 +35,7 @@ export const YandexOAuth = {
 
     return 'https://oauth.yandex.ru/authorize?' + new URLSearchParams(query);
   },
-  auth: (code: string, redirect_uri: string =  __APP_URL__): AxiosResponse =>
+  auth: (code: string, redirect_uri: string = BASE_REDIRECT): AxiosResponse =>
     axiosInstance.post(Routes.AUTH, {
       code,
       redirect_uri,
