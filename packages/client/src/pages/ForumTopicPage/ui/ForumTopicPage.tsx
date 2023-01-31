82d45e512275e9659f74c1da9eb6ea800bfabb48
Violet <eerfeer@yandex.ru>
2023-01-31 22:13:48 +0300

    Add comments

--- packages/client/src/pages/ForumTopicPage/ui/ForumTopicPage.tsx
+++ packages/client/src/pages/ForumTopicPage/ui/ForumTopicPage.tsx
@@ -1,6 +1,53 @@
 import { ArrowBack } from '@/shared/ui/ArrowBack';
+import { ForumComment, ForumCommentProps } from '@/shared/ui/ForumComment';
 import { ForumHeader } from '@/shared/ui/ForumHeader';
 import { useLocation } from 'react-router-dom';
+import css from './ForumTopicPage.module.scss';
+
+const testData: ForumCommentProps[] = [
+  {
+    id: 184441,
+    senderName: 'Elka',
+    senderImgSrc: '',
+    text: 'Omg omg omg omg omg omg omg omg omg omg omg omg omg',
+    date: '20.02.20 20:20',
+  },
+  {
+    id: 184442,
+    senderName: 'Elka',
+    senderImgSrc: '',
+    text: 'Omg omg omg omg omg omg omg omg omg omg omg omg omg',
+    date: '20.02.20 20:20',
+  },
+  {
+    id: 184441,
+    senderName: 'Elka',
+    senderImgSrc: '',
+    text: 'Omg omg omg omg omg omg omg omg omg omg omg omg omg',
+    date: '20.02.20 20:20',
+  },
+  {
+    id: 184441,
+    senderName: 'Elka',
+    senderImgSrc: '',
+    text: 'Omg omg omg omg omg omg omg omg omg omg omg omg omg',
+    date: '20.02.20 20:20',
+  },
+  {
+    id: 184442,
+    senderName: 'Elka',
+    senderImgSrc: '',
+    text: 'Omg omg omg omg omg omg omg omg omg omg omg omg omg',
+    date: '20.02.20 20:20',
+  },
+  {
+    id: 184442,
+    senderName: 'Elka',
+    senderImgSrc: '',
+    text: 'Omg omg omg omg omg omg omg omg omg omg omg omg omg',
+    date: '20.02.20 20:20',
+  },
+];
 
 const ForumTopicPage = () => {
   const { state } = useLocation();
@@ -8,6 +55,11 @@ const ForumTopicPage = () => {
     <>
       <ArrowBack />
       <ForumHeader title={state.topicTitle} />
+      <div className={css.topic_comments}>
+        {testData.map((comment, index) => (
+          <ForumComment key={index} {...comment} />
+        ))}
+      </div>
     </>
   );
 };
---
+++ packages/client/src/shared/ui/ForumComment/ui/ForumComment.tsx
@@ -0,0 +1,29 @@
+import { useAppSelector } from '@/shared/lib/redux';
+import classNames from 'classnames';
+import { ForumUserInfo } from '../../ForumUserInfo';
+import css from './ForumComment.module.scss';
+
+export interface ForumCommentProps {
+  id: number;
+  senderImgSrc: string;
+  senderName: string;
+  text: string;
+  date: string;
+}
+
+export const ForumComment = (props: ForumCommentProps) => {
+  const user = useAppSelector((state) => state.user);
+  const { id, senderImgSrc, senderName, text, date } = props;
+
+  console.log({ user });
+
+  return (
+    <div className={classNames(css.comment, { [css.own]: id === user.id })}>
+      <div className={css.comment__header}>
+        <ForumUserInfo avatarUrl={senderImgSrc} name={senderName} />
+      </div>
+      <span className={css.comment__body}>{text}</span>
+      <time className={css.comment__footer}>{date}</time>
+    </div>
+  );
+};
