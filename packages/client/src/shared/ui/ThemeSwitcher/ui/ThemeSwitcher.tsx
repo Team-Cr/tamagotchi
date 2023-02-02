c5ee2ad9859c1335700cbb3308698105f232ba87
Adel Zaripov <zaripovar1998@gmail.com>
2023-02-02 17:31:53 +0300

    fix review

--- packages/client/src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ packages/client/src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,13 +1,10 @@
-import Paw from '@/shared/assets/images/Paw.gif';
 import { ComponentProps } from '@/shared/ui/types';
 import css from './ThemeSwitcher.module.scss';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const ThemeSwitcher = (props: ComponentProps) => {
   const currentTheme = document.documentElement.dataset.theme;
-  const [toggled, setToggled] = useState(
-    currentTheme ? (currentTheme === 'light' ? true : false) : true,
-  );
+  const [toggled, setToggled] = useState(currentTheme ? currentTheme === 'light' : true);
 
   function setTheme(themeName: string) {
     localStorage.setItem('theme', themeName);
@@ -17,20 +14,19 @@ export const ThemeSwitcher = (props: ComponentProps) => {
   const handleThemeSwitch = useCallback(() => {
     if (localStorage.getItem('theme') === 'dark') {
       setTheme('light');
-      setToggled(!toggled);
     } else {
       setTheme('dark');
-      setToggled(!toggled);
     }
+    setToggled(!toggled);
   }, [toggled]);
 
-  (function () {
+  useEffect(() => {
     if (localStorage.getItem('theme') === 'dark') {
       setTheme('dark');
     } else {
       setTheme('light');
     }
-  })();
+  });
 
   return (
     <div className={css.switcher}>
